2c4f00029e6c482d2f8f8f955bed11f4466d5f22
tanqin <cn.tanqin@qq.com>
2025-07-22 23:33:10 +0800

    feat: 添加 todo svg 图标 && github pages 部署配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { TodoContext } from './TodoContext'
 import { useTodos } from './hooks/useTodos'
 import AddTodo from './components/AddTodo'
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 
 const NoSSRTodoList = dynamic(() => import('./components/TodoList'), { ssr: false })
 
@@ -12,7 +13,9 @@ export default function Home() {
   return (
     <TodoContext.Provider value={todoHook}>
       <div className='absolute left-1/2 -translate-x-1/2 w-full sm:max-w-sm flex flex-col items-center'>
-        <h1 className='font-bold text-3xl m-2.5'>Todo List</h1>
+        <h1 className='flex font-bold text-3xl m-2.5'>
+          <Image className='mr-1' src='/todo.svg' alt='todo' width={20} height={20} /> Todo List
+        </h1>
         <AddTodo />
         <NoSSRTodoList />
       </div>
