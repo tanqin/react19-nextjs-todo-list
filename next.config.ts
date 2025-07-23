6fc2255ffef9edac5f4e235155062753d341dea5
tanqin <cn.tanqin@qq.com>
2025-07-23 14:12:29 +0800

    chore: 修改基础路径配置

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,12 @@ import { name } from './package.json'
 const nextConfig: NextConfig = {
   /* config options here */
   // devIndicators: false,
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${name}/` : '',
-  output: 'export'
+  basePath: process.env.NODE_ENV === 'production' ? `/${name}` : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? `/${name}/` : '',
+  output: 'export', // 启用静态导出
+  images: {
+    unoptimized: true // 静态导出需禁用图片优化
+  }
 }
 
 export default nextConfig
